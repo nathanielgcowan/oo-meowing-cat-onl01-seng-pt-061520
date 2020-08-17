86e551c17e5c95d33c93bb0790e2363721bd75ef
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-17 11:44:38 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Cat
   attr_accessor :name
 
@@ -6,3 +7,20 @@ class Cat
   end
 end
 
+=======
+class Cat
+  
+  def initialize(cats_name)
+    @name = cats_name
+  end
+  def cats_name
+    @name
+  end
+end
+
+class Cat
+  def meow
+    puts "meow!"
+  end
+end
+>>>>>>> c977f48445bc2a8e15482a62bee3aa769eb2a699
